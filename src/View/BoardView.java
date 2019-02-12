bae87fbdb3129f99c964da65a2923dc021efdd99
Xiaoxiao Wu <xwu74@illinois.edu>
2019-02-12 11:11:48 -0600

    add static GUI

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -1,8 +1,10 @@
 package View;
 
+import java.awt.AlphaComposite;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
@@ -10,8 +12,11 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -35,6 +40,7 @@ public class BoardView implements ActionListener{
 	private final int height = 8;
 	private JButton[][] chessBoardSquares = new JButton[width][height];
 	
+
     public BoardView(){
     	
         try {
@@ -44,12 +50,11 @@ public class BoardView implements ActionListener{
         }
         JFrame window = new JFrame("Chess Game");
         window.setSize(500, 500);  
-        
+         
         initializeBoardGrid();
         initializeChessPieces();
-        
+              
         window.getContentPane().add(chessBoard, BorderLayout.CENTER);
-        //window.getContentPane().add(info, BorderLayout.EAST);
         //JPanel myPanel = initializePanel();
         //initializeButton(myPanel);
         //setUpMenu(window);
@@ -58,12 +63,15 @@ public class BoardView implements ActionListener{
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
+    
+    
     /**
      * Initialize the chess board grid panel
      */
     private void initializeBoardGrid(){
     	
         chessBoard = new JPanel(new GridLayout(8,8));
+        chessBoard.setOpaque(false);
         chessBoard.setBorder(new CompoundBorder(
                 new EmptyBorder(8,8,8,8),
                 new LineBorder(Color.BLACK)
@@ -79,52 +87,51 @@ public class BoardView implements ActionListener{
             for (int j = 0; j < height; j++) {
                 chessBoardSquares[i][j] = new JButton();
                 if (Math.abs(i-j)%2==0)
-                	chessBoardSquares[i][j].setBackground(Color.WHITE);
+                	chessBoardSquares[i][j].setBackground(new Color(232,235,239));
                 else
-                	chessBoardSquares[i][j].setBackground(Color.GRAY);
+                	chessBoardSquares[i][j].setBackground(new Color(125,135,150));
                 chessBoardSquares[i][j].setBorder(null);
                 chessBoardSquares[i][j].setOpaque(true);
+                chessBoardSquares[i][j].setBorderPainted(false);
                 chessBoardSquares[i][j].setActionCommand(""+i+j);
                 chessBoard.add(chessBoardSquares[i][j]);
             }
         }
     }
         
-    /**
-     * Fill in the chess board with pieces in their initial positions
-     */
-    private void initializeChessPieces() {
-    	//Image[] blackImages = new Image[16];
-    	String[] blackPieces = {"Rook_black","Knight_black","Bishop_black","Queen_black","King_black","Bishop_black","Knight_black","Rook_black",
-                "Pawn_black","Pawn_black","Pawn_black","Pawn_black","Pawn_black","Pawn_black","Pawn_black","Pawn_black"};
-    	//Image[] whiteImages = new Image[16];
-    	String[] whitePieces = {"Rook_white","Knight_white","Bishop_white","Queen_white","King_white","Bishop_white","Knight_white","Rook_white",
-                "Pawn_white","Pawn_white","Pawn_white","Pawn_white","Pawn_white","Pawn_white","Pawn_white","Pawn_white"};
-    	
-    	for (int i=0; i<8; i++) {
-    		chessBoardSquares[0][i].setIcon(createIcon(blackPieces[i]));
-            chessBoardSquares[1][i].setIcon(createIcon(blackPieces[i+8]));
-            chessBoardSquares[6][i].setIcon(createIcon(whitePieces[i+8]));
-            chessBoardSquares[7][i].setIcon(createIcon(whitePieces[i]));
+   /**
+    * Fill in the chess board with pieces in their initial positions
+    */
+   private void initializeChessPieces() {
+	   
+   	String[] blackPieces = {"bRook","bKnight","bBishop","bQueen","bKing","bBishop","bKnight","bRook",
+               "bPawn","bPawn","bPawn","bPawn","bPawn","bPawn","bPawn","bPawn"};
+   	String[] whitePieces = {"wRook","wKnight","wBishop","wQueen","wKing","wBishop","wKnight","wRook",
+               "wPawn","wPawn","wPawn","wPawn","wPawn","wPawn","wPawn","wPawn"};
+   	
+   	for (int i=0; i<8; i++) {
+   		chessBoardSquares[0][i].setIcon(new ImageIcon(readImage(blackPieces[i])));
+        chessBoardSquares[1][i].setIcon(new ImageIcon(readImage(blackPieces[i+8])));
+        chessBoardSquares[6][i].setIcon(new ImageIcon(readImage(whitePieces[i+8])));
+        chessBoardSquares[7][i].setIcon(new ImageIcon(readImage(whitePieces[i])));
 
-    	}
-    }
+   	}
+   }
     
-    /**
-     * Create icon image based on the image name
-     * @param image_name the name of image(in folder pieces_imgs)
-     * @return the image name's corresponding image icon
-     */
-    private ImageIcon createIcon(String image_name){
+
+   /**
+    * Create icon image based on the image name
+    * @param image_name the name of image(in folder pieces_imgs)
+    * @return the image name's corresponding image icon
+    */
+   private BufferedImage readImage(String image_name) {
         try {
-            String path = "Images/";
-            return new ImageIcon(ImageIO.read(getClass().getResource(path + image_name+".png")).getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH));
-        } catch (Exception e) {
-            System.out.println("Cannot read image");
+            return ImageIO.read(getClass().getResource("Image/" + image_name + ".png"));
+        } catch (IOException e) {
             return null;
         }
     }
-    
+   
   
  
     private void initializeButton(JPanel myPanel) {
@@ -133,12 +140,12 @@ public class BoardView implements ActionListener{
         myPanel.add(button, BorderLayout.SOUTH);
     }
  
-//    private JPanel initializePanel() {
-//        JPanel myPanel = new JPanel();
-//        myPanel.setPreferredSize(new Dimension(500,500));
-//        myPanel.setLayout(new BorderLayout());
-//        return myPanel;
-//    }
+    private JPanel initializePanel() {
+        JPanel myPanel = new JPanel();
+        myPanel.setPreferredSize(new Dimension(500,500));
+        myPanel.setLayout(new BorderLayout());
+        return myPanel;
+    }
  
     private void setUpMenu(JFrame window) {
         JMenuBar menubar = new JMenuBar();
