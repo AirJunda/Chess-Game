57e8ace52dae6ce6fb319b77b437a3329f564168
Xiaoxiao Wu <xwu74@illinois.edu>
2019-02-16 15:35:05 -0600

    start controller code

---
+++ src/Controller/Controller.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+import Game.*;
+import Pieces.*;
+import View.*;
+
+public class Controller{
+	// TODO 
+}
