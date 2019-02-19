76117453ab252409fb8694d41863f5c6dbc1818f
Xiaoxiao Wu <xwu74@illinois.edu>
2019-02-19 03:36:54 -0600

    add button action listener

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -2,12 +2,173 @@ package Controller;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 import Game.*;
+import Game.ChessGame.Player;
 import Pieces.*;
 import View.*;
 
 public class Controller{
-	// TODO 
+	
+	private ChessGame game;
+	private BoardView view;
+	private JButton[][] buttons;
+	
+	 /**
+     * Constructor for the Controller
+     * @param game : current game
+     * @param view : the gui of the board
+     */
+	public Controller(ChessGame game, BoardView view) {
+		this.game = game;
+		this.view = view;
+		buttons = view.getButtons();
+	}
+	
+	
+    /**
+     * Change the selected button background; if already changed(selected), change back to original background color
+     * @param button
+     */
+    public void changeButtonBackground(JButton button) {
+    	// selected, change back 
+    	if (button.getBackground() == Color.yellow) {
+    		 button.setBackground(view.selectedBackGroundColor);
+    		 view.selectedBackGroundColor = null;
+    		 return;
+    	}
+    	
+    	view.selectedBackGroundColor = button.getBackground();
+    	button.setBackground(Color.yellow);
+    	
+    }
+	
+	/**
+	 * Move Piece icon to another location 
+	 * @param e : event
+	 */
+	public void moveAttempt(ActionEvent e) {
+		
+		// nothing selected, double check
+		if (view.selectedButton == null) {
+			return;
+		}
+		
+		JButton button = (JButton) e.getSource();
+		
+		// get original location of the selected piece and its target location
+		int [] loc = getLocation(view.selectedButton.getActionCommand());
+		int [] nloc = getLocation(e.getActionCommand());
+        System.out.println("This piece would like to move to " + nloc[0] + " , " + nloc[1]);
+        
+		boolean isMoved = oneStepMove(loc[0],loc[1],nloc[0],nloc[1]);
+		if (isMoved) {
+			updateIcon(button);
+		}
+		
+	}
+	
+	/** 
+	 * One step of move, check all possible conditions
+	 */
+	public boolean oneStepMove(int x, int y, int nx, int ny) {
+		
+		int width = game.board.getWidth();
+		int height = game.board.getHeight();
+		
+		if (x<0 || x>=width || y<0 || y>=height || nx<0 || nx>=width || ny<0 || ny>=height) {
+			 JOptionPane.showMessageDialog(null,"Invalid Selection: Out Of Bound"); 
+			 return false;
+		}
+			
+		// if the target location is the same as the current location, nothing happened and user may select again
+		if (x==nx && y==ny)
+			return false;
+		
+		Piece p = game.board.getPiece(x, y);
+	
+		
+		if((p.getColor() == Piece.Color.white && game.getPlayer() == Player.playerBlack)){
+			JOptionPane.showMessageDialog(null,"You are player Black, you can only select black chess pieces"); 
+			return false;
+		}
+		if((p.getColor() == Piece.Color.black && game.getPlayer() == Player.playerWhite)){
+			JOptionPane.showMessageDialog(null,"You are player White, you can only select white chess pieces"); 
+			return false;
+		}
+		
+		// valid move
+		if (p.canMove(game.board, nx, ny)) {
+			p.move(game.board,nx,ny);	
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"Invalid Location"); 
+			return false;
+		}
+				
+		game.switchPlayer();	
+		System.out.println("Now it is Player "+ game.getPlayerColor().toString() + "'s turn");
+		
+	
+       
+		return true;
+		// if selected out of bound
+		
+	}
+	
+	public void updateIcon(JButton button) {
+		button.setIcon(view.selectedButton.getIcon());
+		view.selectedButton.setIcon(null);
+	}
+	
+
+	
+	/**
+	 * Undo event actionListener. A player can only undo one step and only after a step was make.
+	 */
+	public void undo() {
+		// TODO
+	}
+	
+	/**
+	 * Show confirmation Message by component to restart game
+	 * @param the current window frame
+	 */
+//	public void restart() {
+//		int reply = JOptionPane.showConfirmDialog(
+//			    view.window,
+//			    game.getPlayer() 
+//			    + " would like to restart the game. "
+//			    + "Do you accept?",
+//			    "Make a Decision",
+//			    JOptionPane.YES_NO_OPTION);
+//		
+//		// YES
+//		if (reply == JOptionPane.YES_OPTION) {
+//			//Chess.restart();
+//			//modelReset();
+//			//TODO
+//		}
+//	}
+	
+
+	/**
+     * string manipulation to get the x, y location of the selected button 
+     * @param s : string of selected button location 
+     * @return An integer array of x,y location 
+     */
+    public int [] getLocation(String s){
+        int [] ret = {-1,-1};
+        ret[0] = Character.getNumericValue(s.charAt(0));
+        ret[1] = Character.getNumericValue(s.charAt(1));
+        return ret;
+    }
+    
+
+
+	
 }
--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -3,6 +3,7 @@ package View;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -26,18 +27,39 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+
+import Controller.*;
+import Game.ChessGame;
  
  
-public class BoardView implements ActionListener{
+public class BoardView {
  
-	private JPanel chessBoard;
-	private final int width = 8;
-	private final int height = 8;
-	private JButton[][] chessBoardSquares = new JButton[width][height];
+	public ChessGame game = new ChessGame(8,8,true);
+	public Controller controller = new Controller(game, this);
+	
+	public JPanel chessBoard;
+	public final int width = 8;
+	public final int height = 8;
+	//public ButtonActionListener bal = new ButtonActionListener();
+	public JButton[][] chessBoardSquares = new JButton[width][height];
+	
+	
+	public JMenuBar menuBar = new JMenuBar();
+	public MenuBarActionListener mal = new MenuBarActionListener();
+	public JMenu menu = new JMenu("Game");
+    public JMenuItem restart = new JMenuItem("Restart");
+	public JMenuItem forfeit = new JMenuItem("Forfeit");
+	public JMenuItem undo = new JMenuItem("Undo");
+	public JMenu scores = new JMenu("Scores");
+	public JMenuItem result = new JMenuItem("Show Results");
+	
+	public java.awt.Color selectedBackGroundColor;
+	public JButton selectedButton;
 	
 
     public BoardView(){
@@ -50,24 +72,24 @@ public class BoardView implements ActionListener{
         JFrame window = new JFrame("Chess Game");
         window.setSize(500, 500);  
          
-        initializeBoardGrid();
-        initializeChessPieces();
+        initimalizeBoardGrid();
+        initimalizeChessPieces();
               
         window.getContentPane().add(chessBoard, BorderLayout.CENTER);
-        //JPanel myPanel = initializePanel();
-        //initializeButton(myPanel);
-        //setUpMenu(window);
+        //initimalizeButton(myPanel);
+        setUpMenu(window);
         //window.setContentPane(myPanel);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
     
     
     
     /**
-     * Initialize the chess board grid panel
+     * Initimalize the chess board grid panel
      */
-    private void initializeBoardGrid(){
+    public void initimalizeBoardGrid(){
     	
         chessBoard = new JPanel(new GridLayout(8,8));
         chessBoard.setOpaque(false);
@@ -89,19 +111,30 @@ public class BoardView implements ActionListener{
                 	chessBoardSquares[i][j].setBackground(new Color(232,235,239));
                 else
                 	chessBoardSquares[i][j].setBackground(new Color(125,135,150));
+                
                 chessBoardSquares[i][j].setBorder(null);
                 chessBoardSquares[i][j].setOpaque(true);
                 chessBoardSquares[i][j].setBorderPainted(false);
                 chessBoardSquares[i][j].setActionCommand(""+i+j);
                 chessBoard.add(chessBoardSquares[i][j]);
+                
+                chessBoardSquares[i][j].addActionListener(new ButtonActionListener());
+              
+                
             }
         }
+        
+
+        
     }
+    
+    
+    
         
    /**
-    * Fill in the chess board with pieces in their initial positions
+    * Fill in the chess board with pieces in their initimal positions
     */
-   private void initializeChessPieces() {
+   public void initimalizeChessPieces() {
 	   
    	String[] blackPieces = {"bRook","bKnight","bBishop","bQueen","bKing","bBishop","bKnight","bRook",
                "bPawn","bPawn","bPawn","bPawn","bPawn","bPawn","bPawn","bPawn"};
@@ -115,55 +148,181 @@ public class BoardView implements ActionListener{
         chessBoardSquares[7][i].setIcon(new ImageIcon(readImage(whitePieces[i])));
 
    	}
+   	
    }
     
 
    /**
-    * Create icon image based on the image name
-    * @param image_name the name of image(in folder pieces_imgs)
-    * @return the image name's corresponding image icon
+    * Create the buffered image
+    * @param image_name: image(file) names
+    * @return buffered image
     */
-   private BufferedImage readImage(String image_name) {
+   public BufferedImage readImage(String image_name) {
         try {
             return ImageIO.read(getClass().getResource("Image/" + image_name + ".png"));
         } catch (IOException e) {
             return null;
         }
     }
+
    
+    /**
+     * set up the menu bar 
+     * @param current window 
+     */
+    public void setUpMenu(JFrame window) {
+    	
+    	window.setJMenuBar(menuBar);
+    	menuBar.add(menu);
+    	menu.add(restart);
+    	restart.addActionListener(mal);
+    	menu.addSeparator();
+        menu.add(forfeit);
+        forfeit.addActionListener(mal);
+        menu.addSeparator();
+        menu.add(undo);
+        undo.addActionListener(mal);
+        
+		menuBar.add(scores);
+		scores.add(result);
+		result.addActionListener(mal);
+
+    }
+    
+    
+    /**
+	 * Creates a new button listener that will take mall
+	 * possible commands that we have programmed.
+	 * @param frame the frame that the actionlistener is anchored to
+	 * @return the actionlistener to take button press input
+	 */
+    public class MenuBarActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        	JMenuItem item = (JMenuItem) e.getSource();
+            String name = item.getText();
+ 
+            if (name.equals("Restart")) {
+            	// TODO
+            	JOptionPane.showMessageDialog(null,
+                        "I was clicked by "+e.getActionCommand(),
+                        "Title here", JOptionPane.INFORMATION_MESSAGE);
+            }
+            
+            if (name.equals("Forfeit")) {
+            	// TODO
+            	JOptionPane.showMessageDialog(null,
+                        "I was clicked by "+e.getActionCommand(),
+                        "Title here", JOptionPane.INFORMATION_MESSAGE);
+            }
+            
+            if (name.equals("Undo")) {
+            	// TODO
+            	JOptionPane.showMessageDialog(null,
+                        "I was clicked by "+e.getActionCommand(),
+                        "Title here", JOptionPane.INFORMATION_MESSAGE);
+            }
+            
+            if (name.equals("Show Results")) {
+            	// TODO
+            	JOptionPane.showMessageDialog(null,
+                        "I was clicked by "+e.getActionCommand(),
+                        "Title here", JOptionPane.INFORMATION_MESSAGE);
+            }
   
- 
-    private void initializeButton(JPanel myPanel) {
-        JButton button = new JButton("Click me");
-        button.addActionListener(this);
-        myPanel.add(button, BorderLayout.SOUTH);
+        }
     }
- 
-    private JPanel initializePanel() {
-        JPanel myPanel = new JPanel();
-        myPanel.setPreferredSize(new Dimension(500,500));
-        myPanel.setLayout(new BorderLayout());
-        return myPanel;
+    
+    /**
+     * Cmalled when button is clicked.
+     */
+    public class ButtonActionListener implements ActionListener {
+    	
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // change the color of the button with icon when clicked
+            JButton button = (JButton) e.getSource();
+            int[] loc = controller.getLocation(e.getActionCommand());
+            System.out.println("button " + loc[0] + " , " + loc[1] + " clicked");
+            
+            
+            // if click on a button where no pieces(icons) are placed, cannot do anything
+            if (button.getIcon() == null && selectedButton == null) 
+            	return;
+            // select an unselected piece(button)
+            else if (button.getIcon() != null && selectedButton == null) {
+            	selectedButton = button;
+            	controller.changeButtonBackground(button);   // wait for next click to move
+            	return;
+            }
+            
+            // move a piece
+            if (selectedButton != null) {
+            	// TODO: MOVE PIECE
+            	controller.moveAttempt(e);
+            	// TODO: UPDATE ICONS
+            	
+            	// after a move, clean indicator variables 
+            	controller.changeButtonBackground(selectedButton);
+            	selectedButton = null;
+            	selectedBackGroundColor = null;
+            }
+            
+            
+
+            
+        }
+
     }
- 
-    private void setUpMenu(JFrame window) {
-        JMenuBar menubar = new JMenuBar();
-        JMenu file = new JMenu("File");
-        JMenuItem open = new JMenuItem("Open");
-        open.addActionListener(this);
-        file.add(open);
-        menubar.add(file);
-        window.setJMenuBar(menubar);
+    
+
+   
+    
+    /**
+     * Getter for the chess board buttons
+     */
+    public JButton[][] getButtons(){ return chessBoardSquares; }
+    
+    /**
+     * Getter for the menu item restart
+     */
+    public JMenuItem getRestart() { return restart; }
+    
+    /**
+     * Getter for the menu item forfeit
+     */
+    public JMenuItem getForfeit() { return forfeit; }
+    
+    /**
+     * Getter for the menu item undo
+     */
+    public JMenuItem getUndo() { return undo; }
+    
+    /**
+     * Getter for the menu item Show Scores
+     */
+    public JMenuItem getScores() { return scores; }
+    
+    /**
+     * Remove mall button (icons) on the board
+     */
+    public void removemallButtons(){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                chessBoardSquares[i][j].setIcon(null);
+                chessBoardSquares[i][j].revalidate();
+            }
+        }
     }
- 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(null,
-                    "I was clicked by "+e.getActionCommand(),
-                    "Title here", JOptionPane.INFORMATION_MESSAGE);
-    }
+
  
     public static void main(String[] args) {
         new BoardView();
     }
+
+
+
+
+
 }
